67cc1aeaa6cf68fbfee215e2c7947e832fcb2f86
Ian Walter <iankwalter@me.com>
2014-05-24 19:48:09 -0400

    Fixing test

--- test/ui/name.spec.js
+++ test/ui/name.spec.js
@@ -2,7 +2,7 @@ describe('angularjs homepage', function() {
   it('should greet the named user', function() {
     browser.get('http://localhost:8080/');
 
-    element(by.model('nameService.name')).sendKeys('Zelda');
+    element(by.className('name-input')).sendKeys('Zelda');
 
     var greeting = element(by.className('name'));
 
