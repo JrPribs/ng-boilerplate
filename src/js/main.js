abe4fec36c50568ec7a6663724f0c6b3f3017666
Ian Walter <iankwalter@me.com>
2014-08-08 14:36:46 -0400

    Adding app.js back in, forgot it's needed to separate app for testing, upgrading angular version, removing unused bower components.

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,9 @@
 requirejs.config(window.requirejsConfig);
 
-define(
-  [
-    'angular',
-    'angular-route',
-    'js/controller/home-controller'
-  ],
-  function(angular) {
-    'use strict';
-
-    angular
-      .module('ng-boilerplate', ['ngRoute', 'ng-boilerplate.home-controller'])
-      .config(['$routeProvider', '$sceProvider', function($routeProvider, $sceProvider) {
-        $routeProvider
-          .when('/', {
-            controller: 'HomeController',
-            templateUrl: 'public/template/home.html'
-          })
-          .otherwise({ redirectTo: '/' });
-
-        // Disables Strict Contextual Escaping for IE8 compatibility
-        $sceProvider.enabled(false);
-      }]);
-
-    angular.bootstrap(document, ['ng-boilerplate']);
-  }
-);
+require([
+  'angular',
+  'js/app'
+], function (angular) {
+  // Needs to be separate from app since we don't want to bootstrap to document when running tests
+  angular.bootstrap(document, ['ng-boilerplate']);
+});
